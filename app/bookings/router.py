da8fbc3df9454e9b6680a3b8bf0cfcffde70319a
Alexey Lisanskiy <alekseylisanskiy@gmail.com>
2024-04-21 23:23:39 +0300

    booking schemas

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,11 +1,16 @@
 from fastapi import APIRouter
 from app.bookings.dao import BookingDAO
+from app.bookings.schemas import SBooking
 
 router = APIRouter(prefix="/bookings", tags=["Бронирования"])
 
 @router.get("")
-async def get_bookings():
+async def get_bookings() -> list[SBooking]:
     return await BookingDAO.find_all()
+
+
+
+
 
 
 
--- app/dao/base.py
+++ app/dao/base.py
@@ -4,9 +4,25 @@ from app.bookings.models import Bookings
 
 class BaseDAO:
     model = None
+
     @classmethod
-    async def find_all(cls):
+    async def find_by_id(cls, model_id: int):
         async with async_session_maker() as session:
-            query = Select(cls.model.__table__.columns)
+            query = Select(cls.model.__table__.columns).filter_by(id=model_id)
+            result = await session.execute(query)
+            return result.mappings().one_or_none()
+
+    @classmethod
+    async def find_one_or_none(cls, **filter_by):
+        async with async_session_maker() as session:
+            query = Select(cls.model.__table__.columns).filter_by(**filter_by)
+            result = await session.execute(query)
+            return result.mappings().one_or_none()
+
+    @classmethod
+    async def find_all(cls, **filter_by):
+        async with async_session_maker() as session:
+            query = Select(cls.model.__table__.columns).filter_by(**filter_by)
             result = await session.execute(query)
             return result.mappings().all()
+
