9dc4b848cfb034730d74cc69aa22b06d562846d7
Alexey Lisanskiy <alekseylisanskiy@gmail.com>
2024-05-09 20:52:47 +0300

    Получение конкретного отеля (опционально, может пригодиться для фронтенда)

--- app/hotels/router.py
+++ app/hotels/router.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter
+from app.database import async_session_maker
+from app.hotels.models import Hotels
+from app.hotels.dao import HotelsDAO
+from sqlalchemy import select
+
 
 router = APIRouter(prefix='/hotels', tags=['/Отели и номера'])
 
 
-@router.get('')
-async def get_hotels():
-    pass
+@router.get('/id/{id}')
+async def get_hotels(hotel_id: int):
+    return await HotelsDAO.find_by_id(hotel_id)
