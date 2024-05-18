1d24eb65c28b4fc9eb5ffdf3266d55f43ee43eed
Alexey Lisanskiy <alekseylisanskiy@gmail.com>
2024-05-18 23:52:51 +0300

    some workings with get my rooms

--- app/hotels/rooms/dao.py
+++ app/hotels/rooms/dao.py
@@ -1,6 +1,12 @@
 from app.dao.base import BaseDAO
 from app.hotels.rooms.models import Rooms
+from app.database import async_session_maker
 
 
 class RoomsDAO(BaseDAO):
     model = Rooms
+
+    @classmethod
+    async def get_my_rooms(cls):
+        async with async_session_maker() as session:
+            pass
