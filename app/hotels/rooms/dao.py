a4043945fca528a61eb04b4be10a4743168f70c0
Alexey Lisanskiy <alekseylisanskiy@gmail.com>
2024-05-19 23:49:51 +0300

    some workings with get rooms

--- app/hotels/rooms/dao.py
+++ app/hotels/rooms/dao.py
@@ -1,5 +1,8 @@
+from datetime import date
+
 from app.dao.base import BaseDAO
 from app.hotels.rooms.models import Rooms
+from app.hotels.models import Hotels
 from app.database import async_session_maker
 
 
@@ -7,6 +10,6 @@ class RoomsDAO(BaseDAO):
     model = Rooms
 
     @classmethod
-    async def get_my_rooms(cls):
+    async def get_rooms(cls, hotel_id: int, date_from: date, date_to: date):
         async with async_session_maker() as session:
             pass
--- app/hotels/rooms/router.py
+++ app/hotels/rooms/router.py
@@ -1,6 +1,11 @@
+from datetime import date
+from sqlalchemy import select
+from app.hotels.models import Hotels
+
 from app.hotels.router import router
 
 
 @router.get('/{hotel_id}/rooms')
-async def get_rooms():
-    pass
+async def get_rooms(hotel_id: int, date_from: date, date_to: date):
+    query = select(Hotels).where(Hotels.id == hotel_id)
+
