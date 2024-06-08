c48ed5c721e62baaec7dbc93487408e58785ce40
Alexey Lisanskiy <alekseylisanskiy@gmail.com>
2024-06-08 21:57:28 +0300

    CORS

--- app/main.py
+++ app/main.py
@@ -34,8 +34,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers",
+                   "Access-Control-Allow-Origin", "Authorization"],
 )
 
 
