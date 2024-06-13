8f95f4af9f0729e1345dc25257e616bbf3bf3fb9
Alexey Lisanskiy <alekseylisanskiy@gmail.com>
2024-06-14 00:54:04 +0300

    celery resize images

--- app/images/router.py
+++ app/images/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, UploadFile
 import aiofiles
 
+from app.tasks.tasks import process_pic
+
 router = APIRouter(
     prefix='/images',
     tags=['Загрузка картинок']
@@ -12,3 +14,4 @@ async def add_hotel_image(name: int, file_to_upload: UploadFile):
     async with aiofiles.open(f'app/static/images/{name}.webp', 'wb+') as file_object:
         file = await file_to_upload.read()
         await file_object.write(file)
+    process_pic.delay(f'app/static/images/{name}.webp')
