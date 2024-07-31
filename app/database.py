802cc7762fc2fae29e3fe69552393e4ab680cde0
Alexey Lisanskiy <alekseylisanskiy@gmail.com>
2024-07-31 23:41:08 +0300

    pytest: setting env

--- app/database.py
+++ app/database.py
@@ -1,8 +1,16 @@
+from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from app.config import settings
 
-engine = create_async_engine(settings.DATABASE_URL, echo=True)
+if settings.MODE == 'TEST':
+    DATABASE_URL = settings.TEST_DATABASE_URL
+    DATABASE_PARAMS = {'poolclass': NullPool}
+else:
+    DATABASE_URL = settings.DATABASE_URL
+    DATABASE_PARAMS = {}
+
+engine = create_async_engine(DATABASE_URL, echo=True, **DATABASE_PARAMS)
 
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
 
