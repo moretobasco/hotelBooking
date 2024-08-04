def3bde9eb2985a23d679236fc55ecc66aa4b907
Alexey Lisanskiy <alekseylisanskiy@gmail.com>
2024-08-04 20:40:28 +0300

    pytest dropping and creating tables

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,4 +1,6 @@
+import asyncio
 import json
+from datetime import datetime
 
 import pytest
 from app.database import Base, async_session_maker, engine
@@ -7,21 +9,55 @@ from app.bookings.models import Bookings
 from app.hotels.models import Hotels
 from app.hotels.rooms.models import Rooms
 from app.users.models import Users
+from sqlalchemy import insert
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope='session', autouse=True)
 async def prepare_database():
     assert settings.MODE == 'TEST'
 
-    async with async_session_maker() as conn:
+    async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
     def open_mock_json(model: str):
-        with open(f'app/tests/mock_{model}.json', 'r') as file:
+        with open(f'app/tests/mock_{model}.json', encoding="utf-8") as file:
             return json.load(file)
 
     hotels = open_mock_json('hotels')
     bookings = open_mock_json('bookings')
     rooms = open_mock_json('rooms')
     users = open_mock_json('users')
+
+    for booking in bookings:
+        booking['date_from'] = datetime.strptime(booking['date_from'], '%Y-%m-%d')
+        booking['date_to'] = datetime.strptime(booking['date_to'], '%Y-%m-%d')
+
+    async with async_session_maker() as session:
+        add_hotels = insert(Hotels).values(hotels)
+        add_rooms = insert(Rooms).values(rooms)
+        add_users = insert(Users).values(users)
+        add_bookings = insert(Bookings).values(bookings)
+
+        '''
+        ВАЖЕН ПОРЯДОК СОЗДАНИЯ ТАБЛИЦ, ИНАЧЕ БУДУТ ОШИБКИ С ВНЕШНИМИ КЛЮЧАМИ:
+        
+        INSERT или UPDATE в таблице "bookings" нарушает ограничение внешнего ключа
+        "bookings_room_id_fkey"
+        '''
+
+        await session.execute(add_hotels)
+        await session.execute(add_rooms)
+        await session.execute(add_users)
+        await session.execute(add_bookings)
+
+        await session.commit()
+
+
+# Взято из документации к pytest-asyncio
+@pytest.fixture(scope="session")
+def event_loop(request):
+    """Create an instance of the default event loop for each test case."""
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
