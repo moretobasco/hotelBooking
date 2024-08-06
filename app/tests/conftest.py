f1a24886991461a1f49540110b6544de03f3efa8
Alexey Lisanskiy <alekseylisanskiy@gmail.com>
2024-08-06 22:40:59 +0300

    httpx

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -11,6 +11,9 @@ from app.hotels.rooms.models import Rooms
 from app.users.models import Users
 from sqlalchemy import insert
 
+from httpx import AsyncClient
+from app.main import app as fastapi_app
+
 
 @pytest.fixture(scope='session', autouse=True)
 async def prepare_database():
@@ -61,3 +64,21 @@ def event_loop(request):
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
+
+
+
+@pytest.fixture(scope="function")
+async def ac():
+    async with AsyncClient(app=fastapi_app, base_url="http://test") as ac:
+        yield ac
+
+
+@pytest.fixture(scope="session")
+async def authenticated_ac():
+    async with AsyncClient(app=fastapi_app, base_url="http://test") as ac:
+        await ac.post("/api/v1/auth/login", json={
+            "email": "test@test.com",
+            "password": "test",
+        })
+        assert ac.cookies["booking_access_token"]
+        yield ac
