a18b3bc7812752e5b2cd1af69f3847e6b3376721
Alexey Lisanskiy <alekseylisanskiy@gmail.com>
2024-08-12 14:12:49 +0300

    black

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -14,20 +14,28 @@ class BookingDAO(BaseDAO):
     @classmethod
     async def add(cls, user_id, room_id, date_from, date_to):
         async with async_session_maker() as session:
-            booked_rooms = select(Bookings).where(
-                and_(
-                    Bookings.room_id == room_id,
-                    Bookings.date_from <= date_to,
-                    Bookings.date_to >= date_from
+            booked_rooms = (
+                select(Bookings)
+                .where(
+                    and_(
+                        Bookings.room_id == room_id,
+                        Bookings.date_from <= date_to,
+                        Bookings.date_to >= date_from,
+                    )
                 )
-            ).cte('booked_rooms')
+                .cte("booked_rooms")
+            )
 
-            get_rooms_left = select(
-                (Rooms.quantity - func.count(booked_rooms.c.room_id)).label('rooms_left')
-            ).select_from(Rooms).join(
-                booked_rooms, booked_rooms.c.room_id == Rooms.id, isouter=True
-            ).where(Rooms.id == room_id).group_by(
-                Rooms.quantity, booked_rooms.c.room_id
+            get_rooms_left = (
+                select(
+                    (Rooms.quantity - func.count(booked_rooms.c.room_id)).label(
+                        "rooms_left"
+                    )
+                )
+                .select_from(Rooms)
+                .join(booked_rooms, booked_rooms.c.room_id == Rooms.id, isouter=True)
+                .where(Rooms.id == room_id)
+                .group_by(Rooms.quantity, booked_rooms.c.room_id)
             )
 
             rooms_left = await session.execute(get_rooms_left)
@@ -37,13 +45,17 @@ class BookingDAO(BaseDAO):
                 get_price = select(Rooms.price).filter_by(id=room_id)
                 price = await session.execute(get_price)
                 price = price.scalar()
-                add_booking = insert(Bookings).values(
-                    room_id=room_id,
-                    user_id=user_id,
-                    date_from=date_from,
-                    date_to=date_to,
-                    price=price
-                ).returning(Bookings)
+                add_booking = (
+                    insert(Bookings)
+                    .values(
+                        room_id=room_id,
+                        user_id=user_id,
+                        date_from=date_from,
+                        date_to=date_to,
+                        price=price,
+                    )
+                    .returning(Bookings)
+                )
 
                 new_booking = await session.execute(add_booking)
                 await session.commit()
@@ -55,12 +67,11 @@ class BookingDAO(BaseDAO):
     @classmethod
     async def get_my_bookings(cls, user: Users):
         async with async_session_maker() as session:
-            query = select(
-                Bookings.__table__.columns,
-                Rooms.__table__.columns
-            ).join(
-                Rooms, Bookings.room_id == Rooms.id, isouter=True
-            ).where(Bookings.user_id == user.id)
+            query = (
+                select(Bookings.__table__.columns, Rooms.__table__.columns)
+                .join(Rooms, Bookings.room_id == Rooms.id, isouter=True)
+                .where(Bookings.user_id == user.id)
+            )
             result = await session.execute(query)
             return result.mappings().all()
 
