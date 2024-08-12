040465489b90d0313c22d13cef3a5cb50fbbc985
Alexey Lisanskiy <alekseylisanskiy@gmail.com>
2024-08-12 14:23:36 +0300

    isort

--- app/main.py
+++ app/main.py
@@ -1,30 +1,23 @@
-from fastapi import FastAPI, Query, Depends
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from typing_extensions import Annotated
-from typing import Union
-from datetime import date
-from pydantic import BaseModel
-
-from app.admin.auth import authentication_backend
-from app.bookings.router import router as router_bookings
-from app.users.models import Users
-from app.users.router import router as router_users
-from app.hotels.router import router as router_hotels
-from app.hotels.rooms.router import router as router_rooms
-from app.pages.router import router as router_pages
-from app.images.router import router as router_images
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
-from fastapi_cache.decorator import cache
-from contextlib import asynccontextmanager
-import logging
-from app.config import settings
-from sqladmin import Admin, ModelView
-from app.database import engine
-from app.admin.views import UsersAdmin, BookingsAdmin, HotelsAdmin, RoomsAdmin
-
 from redis import asyncio as aioredis
+from sqladmin import Admin
+
+from app.admin.auth import authentication_backend
+from app.admin.views import BookingsAdmin, HotelsAdmin, RoomsAdmin, UsersAdmin
+from app.bookings.router import router as router_bookings
+from app.config import settings
+from app.database import engine
+from app.hotels.rooms.router import router as router_rooms
+from app.hotels.router import router as router_hotels
+from app.images.router import router as router_images
+from app.pages.router import router as router_pages
+from app.users.router import router as router_users
 
 
 @asynccontextmanager
@@ -68,40 +61,3 @@ app.add_middleware(
     allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers",
                    "Access-Control-Allow-Origin", "Authorization"],
 )
-
-# class HotelsSearchArgs:
-#     def __init__(
-#             self,
-#             location: str,
-#             date_from: date,
-#             date_to: date,
-#             has_parking: Annotated[Union[bool], Query()] = None,
-#             has_spa: Union[bool] = None,
-#             stars: int = Query(ge=1, le=5, default=None),
-#     ):
-#         self.location = location,
-#         self.date_from = date_from,
-#         self.date_to = date_to,
-#         self.has_parking = has_parking,
-#         self.has_spa = has_spa
-#         self.stars = stars
-#
-#
-# class SHotel(BaseModel):
-#     address: str
-#     name: str
-#     stars: int
-
-
-# @app.get('/hotels', response_model=list[SHotel])
-# def get_hotels(
-#         search_args: HotelsSearchArgs = Depends()
-# ):
-#     hotels = [
-#         {
-#             'address': 'ул. Гагарина, 1, Алтай',
-#             'name': 'Super Hotel',
-#             'stars': 5,
-#         }
-#     ]
-#     return hotels
